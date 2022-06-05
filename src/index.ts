6de9d520efed7d1cee0dce0085892ef8bcaef95d
Razvan Statescu <razvan@statescu.net>
2022-06-05 10:55:57 +0300

    update cli and bump version

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+import chalk from 'chalk';
+import figlet from 'figlet';
 import { argv, exit } from 'process';
 
 import packageJson from '../package.json';
@@ -15,29 +17,35 @@ const COMMANDS = {
 const args = argv;
 const command = args ? args[2] : undefined;
 
-// Show version number
-if (command === "--version" || command === "-v") {
-  console.log(packageJson.version);
-  exit();
-}
+console.log(figlet.textSync("elrond-sft", { horizontalLayout: "default", verticalLayout: "default" }));
+console.log("This is a CLI tool you can use to execute SFT related operations on the Elrond Network.\n");
 
 const availableCommands = Object.values(COMMANDS);
 
 const commandsArray = [...availableCommands, "--version", "-v", "--help", "-h"];
 
-const helpMsg = `========================\nAvailable commands:\n========================\n${commandsArray.join("\n")}`;
+// Show version number
+if (command === "--version" || command === "-v") {
+  console.log(`Version ${packageJson.version}`);
+  exit();
+}
 
+const availableCommandsFn = () => {
+  console.log(chalk.yellow("Available commands:"));
+  console.log(`${commandsArray.join("\n")}`);
+};
+
+// Show help
 if (command === "--help" || command === "-h") {
-  console.log(helpMsg);
+  console.log(`Usage: ${packageJson.name} <command>\n`);
+  availableCommandsFn();
   exit(9);
 }
 
+// Show invalid command error
 if (!command || !Object.values(COMMANDS).includes(command)) {
-  console.log(
-    `====================================================\nPlaese provide a proper command. Available commands:\n====================================================\n${commandsArray.join(
-      "\n"
-    )}`
-  );
+  console.log(chalk.red("Please specify a valid command.") + "\n");
+  availableCommandsFn();
   exit(9);
 }
 
